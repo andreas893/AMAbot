d20c6ce57028b95c6534f5c3bdfa44b8e1ae458e
andreas893 <eaa24andrh@students.eaaa.dk>
2026-09-07 14:01:18 +0200

    ny css og ejs rettelser, messages array oprettet

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const server =  express();
 const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const messages = [];
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { messages });
 });
 
+app.post("/ask", (req, res) => {
+    const question = req.body.question;
+    
+    messages.push({ type: "question", text: question });
+    messages.push({ type: "answer", text: "Jeg leder efter et svar..." });
+
+    res.render("index", { messages });
+})
 
 
 app.listen(port, () => {
