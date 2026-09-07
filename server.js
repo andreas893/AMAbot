9e48fae54f5605898d020a951d2d3907f0a479b7
andreas893 <eaa24andrh@students.eaaa.dk>
2026-09-07 14:29:56 +0200

    tjekpunkt for amabotten trin 13

--- server.js
+++ server.js
@@ -10,18 +10,55 @@ app.use(express.static("public"));
 
 const messages = [];
 
+const answers = [
+    {
+        keywords: ["navn", "hedder", "hvem er du"],
+        answer: "Jeg hedder Andreas. Hvad kunne du ellers tænke dig at vide om mig?"
+    },
+    {
+        keywords: ["bor", "fra"],
+        answer: "Jeg bor i Århus (Risskov), men kommer oprindeligt fra, en lille by tæt på Kolding, Jordrup."
+    },
+    {
+        keywords: ["fritid", "hobby", "kan lide"],
+        answer: "I min fritid kan jeg godt lide at lave lidt forskellige ting. Jeg kan godt lide at løbe, jeg spiller en del computerspil og jeg kan godt lide at læse."
+    }
+];
+
+
+function findAnswer(question) {
+    const normalizedQuestion = question.toLowerCase();
+
+    for (const answerGroup of answers) {
+        const hasMatch = answerGroup.keywords.some((keyword) => normalizedQuestion.includes(keyword));
+
+        if (hasMatch) {
+            return answerGroup.answer;
+        }
+    }
+
+    return "Det kender jeg ikke svaret på endnu.";
+}
+
+
+
 app.get("/", (req, res) => {
-    res.render("index", { messages });
+    res.render("index", { messages, error: ""});
 });
 
 app.post("/ask", (req, res) => {
-    const question = req.body.question;
-    
-    messages.push({ type: "question", text: question });
-    messages.push({ type: "answer", text: "Jeg leder efter et svar..." });
+    const question = req.body.question.trim();
+    let error = "";
 
-    res.render("index", { messages });
-})
+    if (!question) {
+        error = "Skriv et spørgsmål, før du sender"
+    } else {
+        messages.push({ type: "question", text: question });
+          const answer = findAnswer(question);
+          messages.push({ type: "answer", text: answer });
+    }    
+    res.render("index", { messages, error });
+});
 
 
 app.listen(port, () => {
